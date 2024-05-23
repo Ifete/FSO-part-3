626313d59e10e92b912a07182c97e6b19f04d042
Ifete <ireneterra20@gmail.com>
2024-05-23 13:15:25 +0200

    3.3: Backend de la Agenda Telefónica, paso 3

--- index.js
+++ index.js
@@ -30,6 +30,26 @@ app.get('/api/personas', (request, response) => {
     response.json(personas)
 })
 
+app.get('/api/info', (request, response) => {
+
+     let dateInfo = new Date();
+     console.log(dateInfo)
+    response.send(
+        '<p>Phonebooh has info for '+ personas.length + ' people</p><p>'+ dateInfo + '</p>'
+    )
+})
+
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const persona = personas.find(persona => persona.id === id)
+
+    if (persona) {
+        response.json(persona)
+      } else {
+        response.status(404).end()
+      }
+  })
 
 const PORT = 3001
 app.listen(PORT)
