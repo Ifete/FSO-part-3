d6018843c1cd4ee67cd333abce7355f192e18ab5
Ifete <ireneterra20@gmail.com>
2024-05-24 07:59:45 +0200

    3.4: Backend de la Agenda Telefónica, paso 4

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ app.get('/api/persons/:id', (request, response) => {
       }
   })
 
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    personas = personas.filter(persona => persona.id !== id)
+
+    response.status(204).end()
+  })
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
