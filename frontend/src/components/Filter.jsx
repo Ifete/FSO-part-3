4c30f6c564c2de94115215bb5494babd4ba1623b
Ifete <ireneterra20@gmail.com>
2024-05-27 11:57:24 +0200

    deploy backend

--- frontend/src/components/Filter.jsx
+++
@@ -1,8 +0,0 @@
-
-const Filter = ({newFilter, handleFilterChange}) => {
-  return (
-    <p>filter shown with <input value={newFilter} onChange={handleFilterChange}/></p>
-  )
-}
-
-  export default Filter
