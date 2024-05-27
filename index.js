32cdfcbfca9294988b0c418eb03b039f6f20d429
Ifete <ireneterra20@gmail.com>
2024-05-27 11:30:58 +0200

    3.9 Backend de la Agenda Telefónica, paso 9

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 var morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 
 
   morgan.token('body', req => {
