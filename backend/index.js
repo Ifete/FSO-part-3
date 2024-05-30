4b416f2271cee9a5e612c6adcd1220d481228682
Ifete <ireneterra20@gmail.com>
2024-05-30 13:13:01 +0200

    3.15: Base de datos de la Agenda Telefónica, paso 3

--- backend/index.js
+++ backend/index.js
@@ -10,18 +10,18 @@ const Person = require('./models/person')
 app.use(cors())
 
 
-  morgan.token('body', req => {
+morgan.token('body', req => {
     return JSON.stringify(req.body)
-  })
+})
 
-  app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(person => {
         response.json(person)
-      })
+    })
 })
 
 app.get('/api/info', (request, response) => {
@@ -45,18 +45,25 @@ app.get('/api/info', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person)
-      })
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => {
+        console.log(error)
+        response.status(500).send({ error: 'malformatted id' })
+    })
 })
 
-/*
+
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    personas = personas.filter(persona => persona.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-*/
 const generateId = () => {
     return Math.floor(Math.random() * 9999)
 }
@@ -64,15 +71,15 @@ app.post('/api/persons', (request, response) => {
     const body = request.body
     if (body.name === undefined) {
         return response.status(400).json({ error: 'content missing' })
-    }else{
+    } else {
         const person = new Person({
             name: body.name,
             number: body.number,
-          })
+        })
 
-          person.save().then(savedPerson => {
+        person.save().then(savedPerson => {
             response.json(savedPerson)
-          })
+        })
     }
 
 })
