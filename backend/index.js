2a8297c6121b3b1ffa13def382346f50e51650dd
Ifete <ireneterra20@gmail.com>
2024-05-30 13:23:24 +0200

    3.16: Base de datos de la Agenda Telefónica, paso 4

--- backend/index.js
+++ backend/index.js
@@ -6,18 +6,24 @@ var morgan = require('morgan')
 const cors = require('cors')
 app.use(express.static('dist'))
 const Person = require('./models/person')
-
 app.use(cors())
-
-
 morgan.token('body', req => {
     return JSON.stringify(req.body)
 })
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
 
 
 
+//GET
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(person => {
         response.json(person)
@@ -25,48 +31,37 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/info', (request, response) => {
-
     let dateInfo = new Date();
     console.log(dateInfo)
-    response.send(
-        '<p>Phonebooh has info for ' + personas.length + ' people</p><p>' + dateInfo + '</p>'
-    )
-})
-
-app.get('/api/info', (request, response) => {
-
-    let dateInfo = new Date();
-    console.log(dateInfo)
-    response.send(
-        '<p>Phonebooh has info for ' + personas.length + ' people</p><p>' + dateInfo + '</p>'
-    )
+    Person.find({}).then(personas => {
+        response.send(
+            '<p>Phonebooh has info for ' + personas.length + ' people</p><p>' + dateInfo + '</p>'
+        )
+    })
 })
 
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response,next) => {
     Person.findById(request.params.id).then(person => {
         if (person) {
             response.json(person)
         } else {
             response.status(404).end()
         }
-    }).catch(error => {
-        console.log(error)
-        response.status(500).send({ error: 'malformatted id' })
-    })
+    }).catch(error => next(error))
 })
 
-
-app.delete('/api/persons/:id', (request, response) => {
+//DELETE
+app.delete('/api/persons/:id', (request, response,next) => {
     Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-const generateId = () => {
-    return Math.floor(Math.random() * 9999)
-}
+
+//POST
+
 app.post('/api/persons', (request, response) => {
     const body = request.body
     if (body.name === undefined) {
@@ -84,6 +79,9 @@ app.post('/api/persons', (request, response) => {
 
 })
 
+
+
 const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
+app.use(errorHandler)
