2efb74b0c5ffa22dde124104180c8f5aab08cc57
Ifete <ireneterra20@gmail.com>
2024-05-30 10:26:09 +0200

    3.12: Base de datos de línea de comandos

---
+++ backend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack:${password}@ifete.clnpomb.mongodb.net/persons?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const PersonSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', PersonSchema)
+
+if (personName != null) {
+  const person = new Person({
+    name: personName,
+    number: personNumber,
+  })
+
+  person.save().then(result => {
+    console.log('Person saved!')
+    mongoose.connection.close()
+  })
+} else {
+
+  Person.find({}).then(result => {
+    console.log("phonebook")
+    result.forEach(Person => {
+      console.log(Person.name, " ", Person.number)
+    })
+    mongoose.connection.close()
+  })
+
+}
